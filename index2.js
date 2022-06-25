8e12be1c461e0a8a4847b410f0832cab8cd73b48
jeremiahomueti <jeremiahomueti@gmail.com>
2022-06-25 14:58:20 +0100

    update

---
+++ index2.js
@@ -0,0 +1,12 @@
+const numbers = [0, 1,2,3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+let sum = 0;
+
+
+numbers.forEach((values) => sum += values)
+
+console.log("The sum of the numbers is " + sum);
+
+//Have to say I felt really good, computing the two tasks in index.js and index2.js.
+
+//forEach is really satisfying to say the least!
